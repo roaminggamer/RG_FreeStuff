7923ea3de18d6dbc873d7051909a864bfd61e417
Roaming Gamer, LLC. <emaurina@gmail.com>
2014-03-31 15:37:41 -0700

    Added Icons Sampler Kits

---
+++ IconsSampler/Android/main.lua
@@ -0,0 +1,4 @@
+
+local tmp = display.newText( "Icon Sampler - Android", 0, 0, system.nativeFont, 20)
+tmp.x = 160
+tmp.y = 240
---
+++ IconsSampler/iPhone/main.lua
@@ -0,0 +1,4 @@
+
+local tmp = display.newText( "Icon Sampler - iOS", 0, 0, system.nativeFont, 20)
+tmp.x = 160
+tmp.y = 240
