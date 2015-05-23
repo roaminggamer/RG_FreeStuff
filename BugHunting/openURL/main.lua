64da4023c2a4884b50c8a78bf873f09b8fa329da
Roaming Gamer, LLC <emaurina@gmail.com>
2015-05-22 19:01:37 -0700

    Added SSK Sampler - Android Binary

---
+++ BugHunting/openURL/main.lua
@@ -0,0 +1,9 @@
+system.openURL( "https://www.youtube.com/watch?v=SEQPPeP0vsQ" ) 
+--[[
+Results:
+2015.2634 - Windows 7   ==> FAIL (All lower case: https://www.youtube.com/watch?v=seqppep0vsq  )
+2015.2625 - OS X 10.107 ==> SUCCESS
+2015.2625 - iOS 8.2     ==> SUCCESS
+2015.2634 - Android 4.4 ==> SUCCESS
+
+--]]
