4cd58280e6f7e5d01a1bca3295023d3e705699db
Roaming Gamer, LLC <emaurina@gmail.com>
2015-10-09 09:37:36 -0700

    android answer update

--- AskEd/2015/10/lgg4/main.lua
+++ AskEd/2015/10/lgg4/main.lua
@@ -6,6 +6,7 @@ local _R_ = { 1,0,0 }
 local _G_ = { 0,1,0 }
 local _B_ = { 0,0,1 }
 local _Y_ = { 1,1,0 }
+local _P_ = { 1,0,1 }
 
 local common			= require "common"
 local size = 80
@@ -13,6 +14,12 @@ local size = 80
 -- Hide buttons as per best feature supported by this device and OS version
 --
 common.easyAndroidUIVisibility()
+
+local tmp = display.newLine( common.centerX, common.top, common.centerX, common.bottom )
+tmp.strokeWidth = 2
+
+local tmp = display.newLine( common.left, common.centerY, common.right, common.centerY )
+tmp.strokeWidth = 2
 
 local ul = display.newRect( common.left, common.top, size, size )
 ul.anchorX = 0
@@ -36,3 +43,6 @@ local ll = display.newRect( common.left, common.bottom, size, size )
 ll.anchorX = 0
 ll.anchorY = 1
 ll:setFillColor( unpack( _Y_ ) )
+
+local c = display.newRect( common.centerX, common.centerY, size, size )
+c:setFillColor( unpack( _P_ ) )
