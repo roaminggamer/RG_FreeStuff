9752608953ebb7f3941b43d9475e13e7fab7ef5d
Roaming Gamer, LLC <roaminggamer@gmail.com>
2016-12-02 14:50:36 -0800

    Added external DLC for SSK2

---
+++ SSK2/external/randomlua.lua
@@ -0,0 +1,203 @@
+-- =============================================================
+-- Random Number Generators - Credit: https://love2d.org/forums/viewtopic.php?f=5&t=3424
+-- =============================================================
+--   Last Updated: 23 NOV 2016
+-- Last Validated: 
+-- =============================================================
+
+--[[------------------------------------
+RandomLua v0.3.1
+Pure Lua Pseudo-Random Numbers Generator
+Under the MIT license.
+copyright(c) 2011 linux-man
+--]]------------------------------------
+
+--[[
+require('randomlua')
+
+l1 = lcg(0) -- Linear congruential generator (Ansi C params)
+l2 = lcg(0, 'nr') --Linear congruential generator (Numerical recipes params)
+l3 = lcg(0, 'mvc') -- Linear congruential generator (Microsoft Visual C params)
+c1 = mwc(0) -- Multiply-with-carry (Ansi C params)
+c2 = mwc(0, 'nr') -- Multiply-with-carry (Numerical recipes params)
+c3 = mwc(0, 'mvc') -- Multiply-with-carry (Microsoft Visual C params)
+m = twister(0) -- Mersenne twister
+
+for n = 1, 10 do
+   print(string.format("%8d%8d%8d%8d%8d%8d%16d\n", l1:random(0), l2:random(0), l3:random(0), c1:random(0), c2:random(0), c3:random(0), m:random(0)))
+end
+
+]]
+
+local randomlua = {}
+if( not _G.ssk ) then
+	_G.ssk = {}
+end
+_G.ssk.randomlua = randomlua
+
+local mFloor = math.floor
+
+local function normalize(n) --keep numbers at (positive) 32 bits
+	return n % 0x80000000
+end
+
+local function bit_and(a, b)
+	local r = 0
+	local m = 0
+	for m = 0, 31 do
+		if (a % 2 == 1) and (b % 2 == 1) then r = r + 2^m end
+		if a % 2 ~= 0 then a = a - 1 end
+		if b % 2 ~= 0 then b = b - 1 end
+		a = a / 2 b = b / 2
+	end
+	return normalize(r)
+end
+
+local function bit_or(a, b)
+	local r = 0
+	local m = 0
+	for m = 0, 31 do
+		if (a % 2 == 1) or (b % 2 == 1) then r = r + 2^m end
+		if a % 2 ~= 0 then a = a - 1 end
+		if b % 2 ~= 0 then b = b - 1 end
+		a = a / 2 b = b / 2
+	end
+	return normalize(r)
+end
+
+local function bit_xor(a, b)
+	local r = 0
+	local m = 0
+	for m = 0, 31 do
+		if a % 2 ~= b % 2 then r = r + 2^m end
+		if a % 2 ~= 0 then a = a - 1 end
+		if b % 2 ~= 0 then b = b - 1 end
+		a = a / 2 b = b / 2
+	end
+	return normalize(r)
+end
+
+local function seed()
+	--return normalize(tonumber(tostring(os.time()):reverse()))
+	return normalize(os.time())
+end
+
+--Mersenne twister
+local mersenne_twister = {}
+mersenne_twister.__index = mersenne_twister
+
+function mersenne_twister:randomseed(s)
+	if not s then s = seed() end
+	self.mt[0] = normalize(s)
+	for i = 1, 623 do
+		self.mt[i] = normalize(0x6c078965 * bit_xor(self.mt[i-1], mFloor(self.mt[i-1] / 0x40000000)) + i)
+	end
+end
+
+function mersenne_twister:random(a, b)
+	local y
+	if self.index == 0 then
+		for i = 0, 623 do   											
+			--y = bit_or(mFloor(self.mt[i] / 0x80000000) * 0x80000000, self.mt[(i + 1) % 624] % 0x80000000)
+			y = self.mt[(i + 1) % 624] % 0x80000000
+			self.mt[i] = bit_xor(self.mt[(i + 397) % 624], mFloor(y / 2))
+			if y % 2 ~= 0 then self.mt[i] = bit_xor(self.mt[i], 0x9908b0df) end
+		end
+	end
+	y = self.mt[self.index]
+	y = bit_xor(y, mFloor(y / 0x800))
+	y = bit_xor(y, bit_and(normalize(y * 0x80), 0x9d2c5680))
+	y = bit_xor(y, bit_and(normalize(y * 0x8000), 0xefc60000))
+	y = bit_xor(y, mFloor(y / 0x40000))
+	self.index = (self.index + 1) % 624
+	if not a then return y / 0x80000000
+	elseif not b then
+		if a == 0 then return y
+		else return 1 + (y % a)
+		end
+	else
+		return a + (y % (b - a + 1))
+	end
+end
+
+function randomlua.twister(s)
+	local temp = {}
+	setmetatable(temp, mersenne_twister)
+	temp.mt = {}
+	temp.index = 0
+	temp:randomseed(s)
+	return temp
+end
+
+--Linear Congruential Generator
+local linear_congruential_generator = {}
+linear_congruential_generator.__index = linear_congruential_generator
+
+function linear_congruential_generator:random(a, b)
+	local y = (self.a * self.x + self.c) % self.m
+	self.x = y
+	if not a then return y / 0x10000
+	elseif not b then
+		if a == 0 then return y
+		else return 1 + (y % a) end
+	else
+		return a + (y % (b - a + 1))
+	end
+end
+
+function linear_congruential_generator:randomseed(s)
+	if not s then s = seed() end
+	self.x = normalize(s)
+end
+
+function randomlua.lcg(s, r)
+	local temp = {}
+	setmetatable(temp, linear_congruential_generator)
+	temp.a, temp.c, temp.m = 1103515245, 12345, 0x10000  --from Ansi C
+	if r then
+		if r == 'nr' then temp.a, temp.c, temp.m = 1664525, 1013904223, 0x10000 --from Numerical Recipes.
+		elseif r == 'mvc' then temp.a, temp.c, temp.m = 214013, 2531011, 0x10000 end--from MVC
+	end
+	temp:randomseed(s)
+	return temp
+end
+
+-- Multiply-with-carry
+local multiply_with_carry = {}
+multiply_with_carry.__index = multiply_with_carry
+
+function multiply_with_carry:random(a, b)
+	local m = self.m
+	local t = self.a * self.x + self.c
+	local y = t % m
+	self.x = y
+	self.c = mFloor(t / m)
+	if not a then return y / 0x10000
+	elseif not b then
+		if a == 0 then return y
+		else return 1 + (y % a) end
+	else
+		return a + (y % (b - a + 1))
+	end
+end
+
+function multiply_with_carry:randomseed(s)
+	if not s then s = seed() end
+	self.c = self.ic
+	self.x = normalize(s)
+end
+
+function randomlua.mwc(s, r)
+	local temp = {}
+	setmetatable(temp, multiply_with_carry)
+	temp.a, temp.c, temp.m = 1103515245, 12345, 0x10000  --from Ansi C
+	if r then
+		if r == 'nr' then temp.a, temp.c, temp.m = 1664525, 1013904223, 0x10000 --from Numerical Recipes.
+		elseif r == 'mvc' then temp.a, temp.c, temp.m = 214013, 2531011, 0x10000 end--from MVC
+	end
+	temp.ic = temp.c
+	temp:randomseed(s)
+	return temp
+end
+
+return randomlua
---
+++ SSK2/readMe.md
@@ -0,0 +1,9 @@
+# SSK2 (Free Stuff)
+
+This folder contains free 'stuff' to go with [![SSK2](./ssk2_small.png)]  [Get It Now!](https://marketplace.coronalabs.com/vendor/4a8d1f3c-5e88-45e8-b5bf-834f5abe8872)
+
++ [external](https://roaminggamer.github.io/RGDocs/pages/SSK2/external/) - Open source content not included in the SSK2 package.  
+	+ This content was excluded to meet certain requirements of the various marketplaces hosting the SSK2 product.  
+	+ This content is quite useful and can easily be integrated into SSK2.
+	+ To include this in SSK2, follow the directions here: https://roaminggamer.github.io/RGDocs/pages/SSK2/external/#installing-external-content
+
