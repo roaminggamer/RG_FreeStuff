42fcbff9ddb28b57a306810ad8a13bcb27eeb1fd
Roaming Gamer, LLC <roaminggamer@gmail.com>
2017-10-03 09:24:46 -0700

    Texture Memory Management - Updated
    Minor tweak + Zip Created

--- AskEd/2014/10/Texture Memory Management/main.lua
+++ AskEd/2014/10/Texture Memory Management/main.lua
@@ -10,13 +10,6 @@ _G.onWin = ( system.getInfo("platformName") == "Win")
 ----------------------------------------------------------------------
 require "ssk2.loadSSK"
 _G.ssk.init()
-_G.ssk.init( { launchArgs 				= ..., 
-	            enableAutoListeners 	= true,
-	            exportCore 				= true,
-	            exportColors 			= true,
-	            exportSystem 			= true,
-	            exportSystem 			= true,
-	            debugLevel 				= 0 } )
 
 ----------------------------------------------------------------------
 --	2. Initialization
