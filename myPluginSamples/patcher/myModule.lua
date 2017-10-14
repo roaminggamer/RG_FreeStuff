655646538f4ff87681161a6d5543d8dc189dea8f
Roaming Gamer, LLC <roaminggamer@gmail.com>
2017-10-13 22:13:17 -0700

    patcher patch update

--- myPluginSamples/patcher/myModule.lua
+++ myPluginSamples/patcher/myModule.lua
@@ -2,8 +2,9 @@ local m = {}
 local cx,cy = display.contentCenterX, display.contentCenterY
 local mRand = math.random
 function m.run( group )
-	local tmp = display.newRect( group, cx - 200, cy + 400, 120, 120 )
-	transition.to(tmp, { x = tmp.x + 300, time = 2000, transition = easing.outBack } )
+	local tmp = display.newRect( group, cx - display.actualContentWidth/2 + 60, cy, 120, 120 )
+	transition.to(tmp, { x = cx + display.actualContentWidth/2 - 60, time = 2000, transition = easing.outBack } )
 	tmp:setFillColor(1,1,1)
+	display.newText( group, 'patch v3 - scripts.myModule', cx, cy + 160,  'Lato-Black.ttf', 22 )
 end
 return m
