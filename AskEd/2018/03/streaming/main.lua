7ea96544d83003378fcd4efac60813f08762cac7
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-03-16 10:50:26 -0700

    streamingwip

---
+++ AskEd/2018/03/streaming/main.lua
@@ -0,0 +1,21 @@
+-- =============================================================
+-- Minimalistic 'starter' main.lua
+-- =============================================================
+io.output():setvbuf("no")
+display.setStatusBar(display.HiddenStatusBar)
+-- =============================================================
+-- LOAD & INITIALIZE - SSK 2
+-- =============================================================
+require "ssk2.loadSSK"
+_G.ssk.init()
+-- =============================================================
+-- Optionally enable meters to check FPS and Memory usage.
+-- =============================================================
+--ssk.meters.create_fps(true)
+--ssk.meters.create_mem(true)
+-- =============================================================
+
+
+--media.playVideo("http://radiourl.mp3", media.RemoteSource, true, onComplete )
+--local function onPlay( event )
+
