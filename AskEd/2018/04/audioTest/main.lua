b07b0dc03f229da2373c820496923ae24686971c
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-04-03 10:24:06 -0700

    audio test

---
+++ AskEd/2018/04/audioTest/main.lua
@@ -0,0 +1,17 @@
+io.output():setvbuf("no")
+display.setStatusBar(display.HiddenStatusBar)
+-- =====================================================
+-- =====================================================
+
+local track = audio.loadStream( "Discovery Hit.mp3" )
+
+local channel = audio.findFreeChannel(1)
+
+audio.reserveChannels( channel )
+
+audio.play( track, { loops = -1, channel = channel } )
+
+timer.performWithDelay( 2000, 
+	function()
+		audio.setVolume(0)
+	end )
