6bd3c6db9d499ec91c98235092684ac431904c88
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-04-10 08:29:16 -0700

    shooter update
    removed extraneous file

--- AskEd/2018/04/shooterMechanics/scripts/myCC.lua
+++
@@ -1,9 +0,0 @@
--- =============================================================
--- Copyright Roaming Gamer, LLC. 2008-2018 (All Rights Reserved)
--- =============================================================
--- myCC.lua - Collision Settings
--- =============================================================
-local myCC = ssk.cc:newCalculator()
-myCC:addNames( "player", "wall", "trigger", "coin", "gate", "platform" )
-myCC:collidesWith( "player", { "wall", "trigger", "coin", "gate", "platform" } )
-return myCC
