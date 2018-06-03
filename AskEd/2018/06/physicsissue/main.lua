3c02f2a15684ba471ad3b8dc01f3450100586537
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-06-03 12:50:30 -0700

    Updates to askEdStarter
    Added fillW/fillT.png
    Added basic fullw, fullh, left, right.. calcs

--- AskEd/2018/06/physicsissue/main.lua
+++ AskEd/2018/06/physicsissue/main.lua
@@ -62,3 +62,4 @@ end
 
 original()
 --alt()
+
--- AskEd/askEdStarter/main.lua
+++ AskEd/askEdStarter/main.lua
@@ -2,6 +2,14 @@ io.output():setvbuf("no")
 display.setStatusBar(display.HiddenStatusBar)
 -- =====================================================
 -- =====================================================
+local cx     = display.contentCenterX
+local cy     = display.contentCenterY
+local fullw  = display.actualContentWidth
+local fullh  = display.actualContentHeight
+local left   = cx - fullw/2
+local right  = cx + fullw/2
+local top    = cy - fullh/2
+local bottom = cy + fullh/2
 -- == Uncomment following lines if you need  physics
 --local physics = require "physics"
 --physics.start()
