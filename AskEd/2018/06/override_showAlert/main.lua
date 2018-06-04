e1753ff1a4399b4ddf90b37abe36db17946c481c
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-06-04 16:32:24 -0700

    update to override help

--- AskEd/2018/06/override_showAlert/main.lua
+++ AskEd/2018/06/override_showAlert/main.lua
@@ -13,6 +13,7 @@ display.setStatusBar(display.HiddenStatusBar)
 -- "iPhone OS", "Android", "Mac OS X", "tvOS", "Win"
 --
 
+print(system.getInfo("platformName"))
 
 -- 1. DO THIS ONCE BEFORE USING SHOW ALERT:
 --
@@ -20,32 +21,33 @@ display.setStatusBar(display.HiddenStatusBar)
 --
 local _oldShow = native.showAlert
 local function overrideShowAlert( force )
-	if( not force and system.getInfo("platformName") ~= "Mac OS X" ) then
-		_oldShow  = nil
-		-- Do nothing.  Just exit.
-		return
-	end
-
-	native.showAlert = function( ... )
-		local title, message, labels, listener = unpack( arg ) 		
-		--
-		local labelCount = #labels
-		local function _listener( event )
-			event.index = (labelCount - event.index) + 1
-			return listener( event )
+	if( force or
+		 (system.getInfo("platformName") == "Mac OS X" ) or
+		 (system.getInfo("platformName") == "Win" ) )  then
+		native.showAlert = function( ... )
+			local title, message, labels, listener = unpack( arg ) 		
+			--
+			local labelCount = #labels
+			local function _listener( event )
+				event.index = (labelCount - event.index) + 1
+				return listener( event )
+			end
+			--
+			local tmp = {}
+			for i = #labels, 1, -1 do
+				tmp[#tmp+1] = labels[i]
+			end 
+			labels = tmp
+			tmp = nil
+			return _oldShow( title, message, labels, _listener )
 		end
-		--
-		local tmp = {}
-		for i = #labels, 1, -1 do
-			tmp[#tmp+1] = labels[i]
-		end 
-		labels = tmp
-		tmp = nil
-		return _oldShow( title, message, labels, _listener )
+	else
+		_oldShow  = nil		
 	end
 end
---overrideShowAlert( )    -- Normal call
-overrideShowAlert( true ) -- Debug call to test in simulator
+
+overrideShowAlert( )    -- Normal call
+--overrideShowAlert( true ) -- Debug call to test in simulator
 
 
 
@@ -53,14 +55,16 @@ overrideShowAlert( true ) -- Debug call to test in simulator
 -- 2. NOW USE SHOW ALERT ANYWHERE:
 --
 ----[[
+local labels = { "Beta 1", "Alpha 2", "Zeta 3", "Gama 4" }
+
 -- Handler that gets notified when the alert closes
 local function onComplete( event )
 	if ( event.action == "clicked" ) then
 		local i = event.index
-		print("pressed ", i)
+		print("pressed ", labels[i])
 	end
 end
   
 -- Show alert with two buttons
-local alert = native.showAlert( "Corona", "Dream. Build. Ship.", { "1", "2", "3", "4" }, onComplete )
+local alert = native.showAlert( "Corona", "Dream. Build. Ship.", labels, onComplete )
 --]]
