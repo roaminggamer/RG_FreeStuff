77d028e10df39b52804e0e409a9a914628b545df
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-06-17 22:09:48 -0700

    recipe

--- AskEd/2018/06/letterboxCalculator/main.lua
+++ AskEd/2018/06/letterboxCalculator/main.lua
@@ -7,97 +7,116 @@ _G.ssk.init( { measure = false } )
 --ssk.meters.create_mem(true)
 --ssk.misc.enableresListhotHelper("s") 
 -- =====================================================
-local menu = require("menu")
-local createRecipe
-local resList = {
-   {320, 480},
-   {640, 960},
-   {1125, 2436},
-   {2048, 2732},
-	{1200, 1920},
-	{1600, 2560},
-   {1080, 1920},
-   {640, 1136},
-   {750, 1334},
-   {768, 1024},
-	{1536, 2048},	
-	{400, 854},
-	{480, 800},
-	{480, 854},
-	{600, 800},
-	{540, 960},
-	{600, 1024},
-	{720, 1280},
-	{768, 1280},
-	{768, 1366},
-	{800, 1280},
-}
-local function sortRes( a,b )
-	return (a[1] == b[1]) and (a[2] < b[2]) or (a[1] < b[1])
-end
-table.sort( resList, sortRes )
---table.print_r(resList)
---
-local sub = {}
-for i = 1, #resList do
-	sub[i] = resList[i][1] .. " x " .. resList[i][2]
-end
-local function onSelect(event)		
-	local text = event.text
-	local entry
-	local parts = string.split(text," ")
-	for i = 1,#resList do
-		if( resList[i][1] == tonumber(parts[1]) and resList[i][2] == tonumber(parts[3]) ) then
-			entry = resList[i]
-		end
+local function runIt()
+	local group = display.newGroup()
+	local menu = require("menu")
+	local createRecipe
+	local resList = {
+	   {320, 480},
+	   {640, 960},
+	   {1125, 2436},
+	   {2048, 2732},
+		{1200, 1920},
+		{1600, 2560},
+	   {1080, 1920},
+	   {640, 1136},
+	   {750, 1334},
+	   {768, 1024},
+		{1536, 2048},	
+		{400, 854},
+		{480, 800},
+		{480, 854},
+		{600, 800},
+		{540, 960},
+		{600, 1024},
+		{720, 1280},
+		{768, 1280},
+		{768, 1366},
+		{800, 1280},
+	}
+	local function sortRes( a,b )
+		return (a[1] == b[1]) and (a[2] < b[2]) or (a[1] < b[1])
 	end
-	if( not entry ) then return end
-	createRecipe(entry[1], entry[2])	
-end
---
-createRecipe = function( contentW, contentH  )
-	print("Content Area: ", contentW, contentH )
-	local contentAspectRatio = contentW/contentH
-
-	local idealW = contentW
-	local idealH = contentH
-
-	local hBleed = 0
-	local vBleed = 0
-
-	for i = 1, #resList do			
-		local targetW, targetH = resList[i][1], resList[i][2]		
-		local widthRatio = targetW/contentW
-		local heightRatio = targetH/contentH			
-		local scale = math.min( widthRatio, heightRatio )		
-		if( scale * idealW < targetW ) then
-			local scale2 = targetW/(idealW * scale)
-			idealW = math.ceil(idealW * scale2)
-		end
-		if( scale * idealH < targetH ) then
-			local scale2 = targetH/(idealH * scale)
-			idealH = math.ceil(idealH * scale2)
-		end
+	table.sort( resList, sortRes )
+	--table.print_r(resList)
+	--
+	local sub = {}
+	for i = 1, #resList do
+		sub[i] = resList[i][1] .. " x " .. resList[i][2]
 	end
-	if( idealW  % 2 ~= 0) then idealW = idealW + 1 end
-	if( idealH  % 2 ~= 0) then idealH = idealH + 1 end
-	local g = display.newGroup()
-	g.x = 10000
-	g.y = 10000
-	local b = display.newRect( g, idealW/2, idealH/2, idealW, idealH)
-	local s = display.newRect( g, idealW/2, idealH/2, contentW, contentH)
-	b:setFillColor(unpack(_P_))
-	s:setFillColor(unpack(_G_))
-	nextFrame( function() display.save( g, { filename="ideal.png", baseDir=system.DocumentsDirectory, captureOffscreenArea=true} ) end )
-	nextFrame( function() display.remove( g ) end, 100 )
-	print( contentW, contentH, idealW, idealH )
-	return contentW, contentH, idealW, idealH
+	local function onSelect(event)		
+		local text = event.text
+		local entry
+		local parts = string.split(text," ")
+		for i = 1,#resList do
+			if( resList[i][1] == tonumber(parts[1]) and resList[i][2] == tonumber(parts[3]) ) then
+				entry = resList[i]
+			end
+		end
+		if( not entry ) then return end
+		createRecipe(entry[1], entry[2])	
+		local function onOpenFolder()
+			local path = ssk.files.documents.getRoot()			
+			ssk.files.desktop.explore(path)
+		end
+		ssk.misc.easyAlert( "Ideal Recipe Generated",
+					           "Your texture is in the documents sandbox folder.",
+					           { { "OK", nil }, { "Open Folder", onOpenFolder } } ) 
+	end
+	--
+	createRecipe = function( contentW, contentH  )
+		print("Content Area: ", contentW, contentH )
+		local contentAspectRatio = contentW/contentH
+
+		local idealW = contentW
+		local idealH = contentH
+
+		local hBleed = 0
+		local vBleed = 0
+
+		for i = 1, #resList do			
+			local targetW, targetH = resList[i][1], resList[i][2]		
+			local widthRatio = targetW/contentW
+			local heightRatio = targetH/contentH			
+			local scale = math.min( widthRatio, heightRatio )		
+			if( scale * idealW < targetW ) then
+				local scale2 = targetW/(idealW * scale)
+				idealW = math.ceil(idealW * scale2)
+			end
+			if( scale * idealH < targetH ) then
+				local scale2 = targetH/(idealH * scale)
+				idealH = math.ceil(idealH * scale2)
+			end
+		end
+		if( idealW  % 2 ~= 0) then idealW = idealW + 1 end
+		if( idealH  % 2 ~= 0) then idealH = idealH + 1 end
+		local g = display.newGroup()
+		g.x = 10000
+		g.y = 10000
+		local b = display.newRect( g, idealW/2, idealH/2, idealW, idealH)
+		local s = display.newRect( g, idealW/2, idealH/2, contentW, contentH)
+		b:setFillColor(unpack(_P_))
+		s:setFillColor(unpack(_G_))
+		nextFrame( function() display.save( g, { filename="ideal.png", baseDir=system.DocumentsDirectory, captureOffscreenArea=true} ) end )
+		nextFrame( function() display.remove( g ) end, 100 )
+		print( contentW, contentH, idealW, idealH )
+		return contentW, contentH, idealW, idealH
+	end
+	--
+	local menu = menu.Menu{ x = centerX, column_width = 400, columns = { "Select Content Area Resolution", sub } }
+	group:insert(menu)
+	local stash = menu:StashDropdowns() -- temporarily set aside dropdowns to not throw off size calculations
+	menu.y = top + 10
+	menu:RestoreDropdowns(stash)
+	menu:addEventListener("menu_item", onSelect )
+	--
+	--createRecipe( 640, 960)
 end
---
-local menu = menu.Menu{ x = centerX, column_width = 400, columns = { "Select Content Area Resolution", sub } }
-local stash = menu:StashDropdowns() -- temporarily set aside dropdowns to not throw off size calculations
-menu.y = top + 10
-menu:RestoreDropdowns(stash)
-menu:addEventListener("menu_item", onSelect )
---
---createRecipe( 640, 960)
+
+if( fullw == 640 and fullh == 960 ) then 
+	runIt()
+else
+	local tmp = display.newText( "PLEASE VIEW AS: 640 x 960", centerX, centerY - 50, "Poppins-Bold.ttf", 38)
+	local tmp = display.newText( "OR TOOL WILL PRODUCE", centerX, centerY, "Poppins-Bold.ttf", 38)
+	local tmp = display.newText( "INCORRECT RESULTS", centerX, centerY + 50, "Poppins-Bold.ttf", 38)
+end
