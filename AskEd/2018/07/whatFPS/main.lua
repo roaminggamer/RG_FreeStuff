6823584e7ec8180b0f8d5b34f37b63e9cca46a14
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-07-22 17:32:42 -0700

    what fps

---
+++ AskEd/2018/07/whatFPS/main.lua
@@ -0,0 +1,7 @@
+io.output():setvbuf("no")
+display.setStatusBar(display.HiddenStatusBar)
+-- =====================================================
+require "ssk2.loadSSK"
+_G.ssk.init( { measure = false } )
+ssk.meters.create_fps(true)
+ssk.meters.create_mem(true)
