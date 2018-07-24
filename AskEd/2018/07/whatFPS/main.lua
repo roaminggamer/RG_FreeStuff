d35c0ff6f8ee5ef2c106f159b07490440020eae0
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-07-24 11:21:19 -0700

    try before you buy updates

--- AskEd/2018/07/whatFPS/main.lua
+++ AskEd/2018/07/whatFPS/main.lua
@@ -5,3 +5,11 @@ require "ssk2.loadSSK"
 _G.ssk.init( { measure = false } )
 ssk.meters.create_fps(true)
 ssk.meters.create_mem(true)
+
+
+
+local physics = require "physics"
+physics.start()
+
+local rect = ssk.display.newRect(nil, centerX, centerY+100, { w = 200, h = 40}, {bodyType="static"} )
+local ball = ssk.display.newCircle( nil, centerX, centerY-100, { radius = 10 }, { radius = 10, bounce = 1 } )
