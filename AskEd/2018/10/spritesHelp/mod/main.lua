3a74f981a500057176dc47c7c9a1054c30d76fba
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-10-08 21:44:32 -0700

    sprite help in  progress

---
+++ AskEd/2018/10/spritesHelp/mod/main.lua
@@ -0,0 +1,33 @@
+
+Hojadedatos =
+{
+    width = 767,
+   height = 148,
+   numFrames = 60,  
+   sheetContentWidth = 7670,
+   sheetContentHeight = 888,
+}
+
+Hoja1 = graphics.newImageSheet( "HojaIntroAnimacion.png", Hojadedatos )
+
+secuenciadedatos =
+{
+ name="AnimarIntro",
+ start = 1, 
+ count = 60,
+ time=3000,        
+ loopCount = 1,    
+ loopDirection = "forward",    
+}
+
+local function Reproducir( event )
+
+Intro = display.newSprite(Hoja1,secuenciadedatos )
+Intro.x=500
+Intro.y=300
+Intro:setSequence( "AnimarIntro" )
+Intro:play()
+
+end
+  
+timer.performWithDelay( 5000, Reproducir )
