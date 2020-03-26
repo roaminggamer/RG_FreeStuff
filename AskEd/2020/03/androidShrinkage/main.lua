2676c7993b5d359666da191013cc93754968d0b3
Roaming Gamer, LLC <emaurina@gmail.com>
2020-03-25 22:43:42 -0700

    fix for shrinkage w/ experiment steps

--- AskEd/2020/03/androidShrinkage/main.lua
+++ AskEd/2020/03/androidShrinkage/main.lua
@@ -34,11 +34,11 @@ local factoryMgr = ssk.factoryMgr; local soundMgr = ssk.soundMgr
 -- =====================================================
 -- EXAMPLE BEGINS HERE
 -- =====================================================
-ssk.android.easyAndroidUIVisibility()
-display.setDefault( "background", 1, 0, 1 )
-local group = display.newGroup()
-newRect( group, centerX, centerY, { w = fullw, h = fullh, fill = _K_, alpha = 0.95 })
-
+local group 
+local beforeViewableContentHeight
+local afterViewableContentHeight
+local fixScale = 1
+local fixOffset = 0
 --
 -- It is known, that opening a native dialog box in Android wil re-show the navigation bar if it was hidden.
 --
@@ -46,16 +46,167 @@ newRect( group, centerX, centerY, { w = fullw, h = fullh, fill = _K_, alpha = 0.
 -- we can investigate what happens with scaling.
 --
 local function reHide()
+	print( "before rehide", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight  )
     ssk.android.easyAndroidUIVisibility()
+    nextFrame( function() print( "after rehide", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight ) end, 1 )
 end
 
 local function openDialog( )
+	print( "before show dialog", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
 	easyAlert( "Roaming Gamer Says...", 
 	            "Yo!  Navigation bar should be showing.  Want to re-hide it?",
 	             { { "Yes!", reHide }, {"Never Mind", nil } } )
+	nextFrame( function() print( "after show dialog", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight ) end, 1 )	
 end
 
-ssk.easyIFC:presetPush( group, "default", centerX, centerY, 200, 60, "Open Dialog", openDialog )
+local function original()
+	group = display.newGroup()
+	ssk.android.easyAndroidUIVisibility()
+	display.setDefault( "background", 1, 0, 1 )
+	newRect( group, centerX, centerY, { w = fullw, h = fullh, fill = _K_, alpha = 0.95 })
+	ssk.easyIFC:presetPush( group, "default", centerX, centerY, 200, 60, "Open Dialog", openDialog )
+end
+
+
+local function experiment1()
+	beforeViewableContentHeight = display.pixelHeight
+	print( "startup", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, display.contentScaleY, display.topStatusBarContentHeight  )
+	group = display.newGroup()
+	ssk.android.easyAndroidUIVisibility()
+	nextFrame( 
+		function()
+			display.setDefault( "background", 1, 0, 1 )
+			newRect( group, centerX, centerY, { w = fullw, h = fullh, fill = _K_, alpha = 0.95 })			
+			ssk.easyIFC:presetPush( group, "default", centerX, centerY, 200, 60, "Open Dialog", openDialog )
+			print( "done drawing", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+			afterViewableContentHeight = display.pixelHeight
+			fixScale = afterViewableContentHeight/beforeViewableContentHeight
+			fixOffset = afterViewableContentHeight - beforeViewableContentHeight
+			print("Fix scale: ", fixScale)
+			print("Fix offset: ", fixOffset)
+			group:scale(fixScale,fixScale)
+			group.y = -fixOffset
+			print( "done fix scaling", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+		end, 1000 )
+end
+
+
+local function experiment2()
+	beforeViewableContentHeight = display.safeActualContentHeight	
+	print( "startup", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, display.contentScaleY, display.topStatusBarContentHeight  )
+	group = display.newGroup()
+	ssk.android.easyAndroidUIVisibility()
+	nextFrame( 
+		function()
+			display.setDefault( "background", 1, 0, 1 )
+			newRect( group, centerX, centerY, { w = fullw, h = fullh, fill = _K_, alpha = 0.95 })			
+			ssk.easyIFC:presetPush( group, "default", centerX, centerY, 200, 60, "Open Dialog", openDialog )
+			print( "done drawing", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+			afterViewableContentHeight = display.safeActualContentHeight
+			fixScale = afterViewableContentHeight/beforeViewableContentHeight
+			fixOffset = afterViewableContentHeight - beforeViewableContentHeight
+			print("Fix scale: ", fixScale)
+			print("Fix offset: ", fixOffset)
+			group:scale(fixScale,fixScale)
+			group.y = -fixOffset/2
+			print( "done fix scaling", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+		end, 1000 )
+end
+
+
+local function experiment3()
+	fixScale = nil
+	beforeViewableContentHeight = display.safeActualContentHeight	
+	print( "startup", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, display.contentScaleY, display.topStatusBarContentHeight  )
+
+	local function onResize( event )
+	    print('resized!',event)
+	    if( fixScale ) then
+			group:scale(fixScale,fixScale)
+			group.y = -fixOffset/2
+			print( "done fix scaling", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+	    else
+			afterViewableContentHeight = display.safeActualContentHeight
+			fixScale = afterViewableContentHeight/beforeViewableContentHeight
+			fixOffset = afterViewableContentHeight - beforeViewableContentHeight
+			print("Fix scale: ", fixScale)
+			print("Fix offset: ", fixOffset)
+			group:scale(fixScale,fixScale)
+			group.y = -fixOffset/2
+			print( "done first fix scaling", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+	    end
+	end	 
+	Runtime:addEventListener( "resize", onResize )	
+	
+	group = display.newGroup()
+	ssk.android.easyAndroidUIVisibility()
+	display.setDefault( "background", 1, 0, 1 )
+	newRect( group, centerX, centerY, { w = fullw, h = fullh, fill = _K_, alpha = 0.95 })			
+	ssk.easyIFC:presetPush( group, "default", centerX, centerY, 200, 60, "Open Dialog", openDialog )
+	print( "done drawing", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+end
 
 
 
+local function experiment4()
+	local currentStage = display.getCurrentStage()
+	fixScale = nil
+	beforeViewableContentHeight = display.safeActualContentHeight	
+	print( "startup", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, display.contentScaleY, display.topStatusBarContentHeight  )
+
+	local function onResize( event )
+	    print('resized!',event)
+	    if( fixScale ) then
+			currentStage:scale(fixScale,fixScale)
+			currentStage.y = -fixOffset/2
+			print( "done fix scaling", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+	    else
+			afterViewableContentHeight = display.safeActualContentHeight
+			fixScale = afterViewableContentHeight/beforeViewableContentHeight
+			fixOffset = afterViewableContentHeight - beforeViewableContentHeight
+			print("Fix scale: ", fixScale)
+			print("Fix offset: ", fixOffset)
+			currentStage:scale(fixScale,fixScale)
+			currentStage.y = -fixOffset/2
+			print( "done first fix scaling", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+	    end
+	end	 
+	Runtime:addEventListener( "resize", onResize )	
+	
+	group = display.newGroup()
+	ssk.android.easyAndroidUIVisibility()
+	display.setDefault( "background", 1, 0, 1 )
+	newRect( group, centerX, centerY, { w = fullw, h = fullh, fill = _K_, alpha = 0.95 })			
+	ssk.easyIFC:presetPush( group, "default", centerX, centerY, 200, 60, "Open Dialog", openDialog )
+	print( "done drawing", display.actualContentHeight, display.viewableContentHeight, display.pixelHeight, display.safeActualContentHeight, group.yScale, display.contentScaleY, display.topStatusBarContentHeight )
+end
+
+local function androidShrinkageFix()
+	local beforeViewableContentHeight
+	local afterViewableContentHeight
+	local fixScale = 1
+	local fixOffset = 0
+	local currentStage = display.getCurrentStage()
+	fixScale = nil
+	beforeViewableContentHeight = display.safeActualContentHeight	
+	local function onResize( event )
+	    print('resized!',event)
+	    if( fixScale ) then
+			currentStage:scale(fixScale,fixScale)
+			currentStage.y = -fixOffset/2
+	    else
+			afterViewableContentHeight = display.safeActualContentHeight
+			fixScale = afterViewableContentHeight/beforeViewableContentHeight
+			fixOffset = afterViewableContentHeight - beforeViewableContentHeight
+			currentStage:scale(fixScale,fixScale)
+			currentStage.y = -fixOffset/2
+	    end
+	end	 
+	Runtime:addEventListener( "resize", onResize )	
+end
+
+
+-- experiment4()
+
+androidShrinkageFix()
+original()
