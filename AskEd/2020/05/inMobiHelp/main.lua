de5efc2cb7ddcde9456801fa3581f2740981cce8
Roaming Gamer, LLC <emaurina@gmail.com>
2020-05-22 22:38:10 -0700

    inMobi help

---
+++ AskEd/2020/05/inMobiHelp/main.lua
@@ -0,0 +1,65 @@
+io.output():setvbuf("no")
+display.setStatusBar(display.HiddenStatusBar)
+-- =====================================================
+require "ssk2.loadSSK"
+_G.ssk.init( { measure = false } )
+-- =====================================================
+--ssk.meters.create_fps(true)
+--ssk.meters.create_mem(true)
+--ssk.misc.enableScreenshotHelper("s") 
+--ssk.easyIFC.generateButtonPresets( nil, true )
+-- =====================================================
+
+-- =============================================================
+-- Localizations
+-- =============================================================
+-- Commonly used Lua & Corona Functions
+local getTimer = system.getTimer; local mRand = math.random
+local mAbs = math.abs; local mFloor = math.floor; local mCeil = math.ceil
+local strGSub = string.gsub; local strSub = string.sub
+-- Common SSK Features
+local newCircle = ssk.display.newCircle;local newRect = ssk.display.newRect
+local newImageRect = ssk.display.newImageRect;local newSprite = ssk.display.newSprite
+local quickLayers = ssk.display.quickLayers
+local easyIFC = ssk.easyIFC;local persist = ssk.persist
+local isValid = display.isValid;local isInBounds = ssk.easyIFC.isInBounds
+local normRot = math.normRot;local easyAlert = ssk.misc.easyAlert
+-- SSK 2D Math Library
+local addVec = ssk.math2d.add;local subVec = ssk.math2d.sub;local diffVec = ssk.math2d.diff
+local lenVec = ssk.math2d.length;local len2Vec = ssk.math2d.length2;
+local normVec = ssk.math2d.normalize;local vector2Angle = ssk.math2d.vector2Angle
+local angle2Vector = ssk.math2d.angle2Vector;local scaleVec = ssk.math2d.scale
+local RGTiled = ssk.tiled; local files = ssk.files
+local factoryMgr = ssk.factoryMgr; local soundMgr = ssk.soundMgr
+
+local back = newImageRect( nil, centerX, centerY, "protoBackX.png", 
+									{ w = 720,  h = 1386, rotation = fullw>fullh and 90 } )
+
+
+local inmobi_helpers = require "inmobi_helpers"
+
+-- Get full debug output from 'inmobi_helpers' library.
+inmobi_helpers.setDebugLevel(3) 
+
+-- Provide an appropriate id for each OS and ad type
+inmobi_helpers.setID( 'android', 	'banner', 		'add_your_android_banner_ID' )
+inmobi_helpers.setID( 'android',	'interstitial', 'add_your_android_interstitial_ID' )
+
+-- inmobi_helpers.setID( 'ios', 		'banner', 		'add_your_ios_banner_ID' )
+-- inmobi_helpers.setID( 'ios', 		'interstitial', 'add_your_ios_interstitial_ID' )
+
+local function onInit( event )
+  print( "Init phase completed!" )
+  -- table.dump(event)
+  nextFrame( function() inmobi_helpers.loadInterstitial() end )
+end
+inmobi_helpers.setPhaseCB( "init", onInit, true )
+
+local function onLoaded( event )
+  print( "Loaded phase completed!" )
+  -- table.dump(event)
+  nextFrame( function() inmobi_helpers.showInterstitial() end )
+end
+inmobi_helpers.setPhaseCB( "loaded", onLoaded, true )
+
+inmobi_helpers.init( "account_id_here", 500, "debug" )
