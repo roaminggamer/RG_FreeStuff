03b9236812351f469d7c136959b1a1d6a522b688
Roaming Gamer, LLC <emaurina@gmail.com>
2020-09-02 10:59:34 -0700

    sprite help

---
+++ AskEd/2020/09/sprites/main.lua
@@ -0,0 +1,88 @@
+-- =============================================================
+-- Copyright Roaming Gamer, LLC. 2009-2020
+-- =============================================================
+-- =============================================================
+-- main.lua
+-- =============================================================
+
+----------------------------------------------------------------------
+--  Misc Configuration & Initialization
+----------------------------------------------------------------------
+display.setStatusBar(display.HiddenStatusBar)  
+system.activate("multitouch")
+--io.output():setvbuf("no") 
+math.randomseed(os.time());
+
+
+local pinkyInfo 	= require "pinky"
+local pinkySheet 	= graphics.newImageSheet("pinky.png", pinkyInfo:getSheet() )
+local pinkySeqData = 
+	{
+		{ name = "idle", frames = {2}, time = 1000, loopCount = 0, loopDirection = "forward"}, 
+		{ name = "jump", frames = {1}, time = 1000, loopCount = 0, loopDirection = "forward"}, 
+		{name = "rightwalk", frames = {3,4,5,6,7,8,9,10,11,12,13 }, time = 500, loopCount = 0, loopDirection = "forward"}, 
+		{name = "leftwalk", frames = {3,4,5,6,7,8,9,10,11,12,13 }, time = 500, loopCount = 0, loopDirection = "forward"}, 
+	}
+
+
+-- Create sprite, play 'idle'
+local tmp = display.newSprite( pinkySheet, pinkySeqData )
+tmp.x = display.contentCenterX - 300
+tmp.y = display.contentCenterY
+tmp:setSequence( "idle" )
+tmp:play()
+display.newText(  "idle", tmp.x, tmp.y + 70, native.systemFontBold, 30 )
+
+
+
+-- Create sprite, play 'jump'
+local tmp = display.newSprite( pinkySheet, pinkySeqData )
+tmp.x = display.contentCenterX - 100
+tmp.y = display.contentCenterY
+tmp:setSequence( "jump" )
+tmp:play()
+display.newText(  "jump", tmp.x, tmp.y + 70, native.systemFontBold, 30 )
+
+
+
+-- Create sprite, play 'rightwalk'
+local tmp = display.newSprite( pinkySheet, pinkySeqData )
+tmp.x = display.contentCenterX + 100
+tmp.y = display.contentCenterY
+tmp:setSequence( "rightwalk" )
+tmp:play()
+display.newText(  "rightwalk", tmp.x, tmp.y + 70, native.systemFontBold, 30 )
+
+
+
+-- Create sprite, play 'leftwalk'
+local tmp = display.newSprite( pinkySheet, pinkySeqData )
+tmp.x = display.contentCenterX + 300
+tmp.y = display.contentCenterY
+tmp.xScale = -1
+tmp:setSequence( "leftwalk" )
+tmp:play()
+display.newText(  "leftwalk", tmp.x, tmp.y + 70, native.systemFontBold, 30 )
+
+
+
+
+
+local sonicInfo 	= require "sonic"
+local sonicSheet 	= graphics.newImageSheet("sonic.png", sonicInfo:getSheet() )
+local sonicSeqData = 
+	{
+		{name = "explode", frames = {1,2,3,4,5}, time = 500, loopCount = 0, loopDirection = "forward"}, 
+	}
+
+-- Create sprite, play 'leftwalk'
+local tmp = display.newSprite( sonicSheet, sonicSeqData )
+tmp.x = display.contentCenterX 
+tmp.y = display.contentCenterY + 200
+tmp.xScale = 3
+tmp.yScale = 3
+tmp:setSequence( "explode" )
+tmp:play()
+display.newText(  "sonic (explode)", tmp.x, tmp.y + 70, native.systemFontBold, 30 )
+
+
